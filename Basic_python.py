15d9fd64103d6049dd82221c71c7b28292dc4228
priyanshu5179 <priyanshuyadav876585@gmail.com>
2026-03-01 23:55:57 +0530

    use conditional satatement

--- Basic_python.py
+++ Basic_python.py
@@ -1,8 +1,11 @@
-a=234
+'''a=234
 print(a)
-print(type(a))
-a=234
-print(a)
-print(type(a))a=234
-print(a)
-print(type(a))
+'''
+nums = [5, 2, 8, 1, 4]
+
+for i in range(len(nums)):
+    count = 0
+    for j in range(len(nums)):
+        if nums[j] < nums[i]:
+            count += 1
+    print(f"Numbers less than {nums[i]} = {count}")
